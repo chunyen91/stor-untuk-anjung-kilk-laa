047efe61995cadef296a0c2114fa16ffb221f394
chunyen91 <32806435+chunyen91@users.noreply.github.com>
2017-12-22 17:20:16 +0800

    Create pptp.sh

---
+++ addd/pptp.sh
@@ -0,0 +1,29 @@
+#install PPTP
+apt-get -y install pptpd
+cat > /etc/ppp/pptpd-options <<END
+name pptpd
+refuse-pap
+refuse-chap
+refuse-mschap
+require-mschap-v2
+require-mppe-128
+ms-dns 8.8.8.8
+ms-dns 8.8.4.4
+proxyarp
+nodefaultroute
+lock
+nobsdcomp
+END
+
+cat > /etc/pptpd.conf <<END
+option /etc/ppp/pptpd-options
+logwtmp
+localip 10.1.0.1
+remoteip 10.1.0.5-100
+END
+
+cat >> /etc/ppp/ip-up <<END
+ifconfig ppp0 mtu 1400
+END
+mkdir /var/lib/premium-script
+/etc/init.d/pptpd restart
